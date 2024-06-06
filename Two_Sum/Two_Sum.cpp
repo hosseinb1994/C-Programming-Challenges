33c330818ea01e8e26a2e9075168752d633fb5f0
Hossein Baghaei <hosseinb444@gmail.com>
2024-06-06 14:11:03 +0200

    Two_sum added.

---
+++ Two_Sum/Two_Sum.cpp
@@ -0,0 +1,29 @@
+﻿extern "C" {
+	#include "Two_Sum.h"
+}
+
+void Two_Sum(int num[], int target, int array_size);
+
+int main()
+{
+	int num[] = {2,7,11,15,40,4};
+	int target = 2;
+	int array_size = sizeof(num) / sizeof(num[0]);
+	Two_Sum(num, target, array_size);
+
+	return 0;
+}
+
+void Two_Sum(int num[], int target, int array_size) {
+	int res = 0;
+	for (int i = 0; i < array_size - 1; i++) {
+		for (int j = 0; j < array_size - i; j++) {
+			res = num[i] + num[j + i];
+			if (res == target) {
+				printf("num is [%d,%d]\n", i, j+i);
+				break;
+			}
+		}
+		res = 0;
+	}
+}
